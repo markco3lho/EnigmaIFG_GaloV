3d8d29c3405a9b80efe133af1dfba34093f55ab2
Mark Coelho <marcos.c4051@gmail.com>
2021-06-19 19:42:39 -0300

    Edição Menu
    Música de fundo editada
    Finalização da logo

---
+++ scripits/buttoncreditos.gd
@@ -0,0 +1,22 @@
+extends Button
+
+func _on_Button_mouse_entered():
+	$menu_navigative.play()
+	$sprite.modulate.r = 0.7
+	$sprite.modulate.g = 0.7
+	$sprite.modulate.b = 0.7
+	pass 
+
+func _on_Button_mouse_exited():
+	pass 
+	$menu_navigative.play()
+	$sprite.modulate.r = 1
+	$sprite.modulate.g = 1
+	$sprite.modulate.b = 1
+func _on_Button_pressed():
+	$menu_navigative.play()
+	$sprite.modulate.r = 0.5
+	$sprite.modulate.g = 0.5
+	$sprite.modulate.b = 0.5
+
+	pass 
